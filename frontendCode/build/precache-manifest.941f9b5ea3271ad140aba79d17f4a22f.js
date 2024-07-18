80b7952485d51e46db910a91ee592ade8d1f5a32
ambalica <amby91@gmail.com>
2024-07-18 21:19:36 +1000

    remove submodule

---
+++ frontendCode/build/precache-manifest.941f9b5ea3271ad140aba79d17f4a22f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248532fa3b30911b0921d6f9d8cc1792",
+    "url": "/index.html"
+  },
+  {
+    "revision": "883295d074c443930758",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "f3cc4e95578501f0d4f8",
+    "url": "/static/js/2.2507f3a9.chunk.js"
+  },
+  {
+    "revision": "e5a7172ac4614a7d76bf",
+    "url": "/static/js/3.c1a7ea53.chunk.js"
+  },
+  {
+    "revision": "883295d074c443930758",
+    "url": "/static/js/main.53469287.chunk.js"
+  },
+  {
+    "revision": "68f7250f6a5f6ca471d7",
+    "url": "/static/js/runtime~main.ef4b6af8.js"
+  }
+]);
